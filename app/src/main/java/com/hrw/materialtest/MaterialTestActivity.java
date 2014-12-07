83cc16d97f8b78def11fd43c2fad58229e2e3c70
gt212345 <wuheiru.5203@gmail.com>
2014-12-07 13:18:24 +0800

    Adjust themes xml

--- app/src/main/java/com/hrw/materialtest/MaterialTestActivity.java
+++ app/src/main/java/com/hrw/materialtest/MaterialTestActivity.java
@@ -3,6 +3,7 @@ package com.hrw.materialtest;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +17,10 @@ public class MaterialTestActivity extends ActionBarActivity implements View.OnCl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_test_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
         intent = (Button)findViewById(R.id.intent);
         intent.setOnClickListener(this);
 
